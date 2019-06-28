3e34c3cd8d63b8a0a5f4677bc1d2de14d01ec502
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-06-28 10:54:44 -0400

    Wrote static methods for a JS class

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let newString = string[0].toUpperCase() + string.slice(1)
+    return newString
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'' ']+/g, '')
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let stringArray = string.split(' ')
+    let firstWord = stringArray[0]
+    let capitalizedFirstWord = firstWord[0].toUpperCase() + firstWord.slice(1)
+    let followingWords = stringArray.slice(1)
+
+    let newStringArray = []
+    followingWords.forEach (word => {
+      if (exceptions.includes(word)) {
+        newStringArray.push(word)
+      } else {
+        let capitalizedWord = word[0].toUpperCase() + word.slice(1)
+        newStringArray.push(capitalizedWord)
+      }
+    })
+    return capitalizedFirstWord + ' ' + newStringArray.join(' ')
+  }
 }
+
+Formatter.capitalize('jenny')
+Formatter.titleize('a tale of two cities')
