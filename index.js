f499bf713fc25cc7ad0c7d20a9aad946a8181f01
Peter Viss <peter.c.viss@biola.edu>
2019-06-28 13:06:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
-  //add static methods here
-}
+  static capitalize(string){
+     return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(str){
+    str = str.replace(/[^A-Za-z0-9 '-]/g, '' );
+    return str;
+  }
+
+  static titleize(string){
+    let newArray = []
+    let badWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let nuArray = string.split(' ')
+    for (let i = 0; i < nuArray.length; i++){
+
+      if (i == 0) {
+        newArray.push(this.capitalize(nuArray[i]))
+      }else{
+      if (badWords.includes(nuArray[i])){ 
+         
+          newArray.push(nuArray[i])
+         
+      } 
+      else {
+         newArray.push(this.capitalize(nuArray[i]))
+        
+      }
+      }
+    }
+      return newArray.join(" ")
+      
+    }
+      
+  }
